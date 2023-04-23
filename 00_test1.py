519f40c2fc26b21733512b74aad5cea9d4bdd298
ramiromd <ramiromd@DESKTOP-4MP4LE0>
2023-04-23 02:42:52 -0600

    Se agrega el primer archivo de la carpeta de aprendizaje
    Introduccion a python
    En este archivo se aborda una breve introduccion con la funcion print, type y tipos de datos basicos

---
+++ 00_test1.py
@@ -0,0 +1,11 @@
+print("Hola python")
+
+
+# Consulta el tipo de dato
+print(type("Hola python"))  # Tipo 'str'
+print(type("Soy un dato str"))  # Tipo 'str'
+print(type(5))  # Tipo 'int'
+print(type(3.14))  # Tipo 'float'
+print(type(True))  # Tipo 'bool'
+print(type(1 + 3j))  # Tipo 'complex'
+5+2
