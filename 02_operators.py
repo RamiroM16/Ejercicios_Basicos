263b2fac9a0a19e4094a34193dd6d2123fbd77b5
ramiromd <ramiromd@DESKTOP-4MP4LE0>
2023-04-28 21:57:42 -0600

    Se agrega el fichero 02_operators.py

---
+++ 02_operators.py
@@ -0,0 +1,47 @@
+# Arithmetic operators
+
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 / 4)
+
+print(10 % 2)
+print(10 // 3)
+print(2 ** 3 + 3 - 7 / 1 // 4)
+
+print("Hola" + "Python")
+print("Hola " + "Python " + "que tal")
+# print("Hola" + 5)             No se pueden combinar tipos
+print("Hola " + str(5))
+
+my_int = 2.5 * 2
+print("Hola " * 5)
+
+# print("Hola " * my_int)       No se puede multiplicar por un flotante
+
+print("Hola " * int(my_int))
+
+# Comparison operators
+
+print(3 > 4)
+print(3 < 4)
+print(3 >= 4)
+print(3 <= 4)
+print(3 == 4)
+print(3 != 4)
+print(3 > 4 == 2)
+
+print("Separacion")
+
+print("Hola" > "python")
+print("Hola" < "python")
+print("Hola" >= "Zola")  # alphabetical Ordination
+print("Hola" <= "python")
+print("Hola" == "python")
+print("Hola" != "python")
+
+# Logic operators
+print("logic operators")
+print(3 > 4 and "Hola" > "Pyhton")
+print(3 > 4 or "Hola" > "Pyhton")
+print(not (3 > 4))
