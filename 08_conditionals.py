868be33f88ce0dc4b9e0370784ff139c958da896
ramiromd <ramiromd@DESKTOP-4MP4LE0>
2023-05-01 06:47:04 -0600

    Se agrega el fichero 08_conditionals.py

---
+++ 08_conditionals.py
@@ -0,0 +1,14 @@
+### Conditionals ###
+
+my_condition = True
+if my_condition:
+    print("Se ejecuta la condicion del if")
+
+my_condition = 15
+if my_condition >= 11 and my_condition < 20:
+    print("Es mayor o igual a 11 y menor que 20")
+else:
+    print("Es menor a 11 o mayor o igual a 20")
+
+
+print("La ejecucion continua")
