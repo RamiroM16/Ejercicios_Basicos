b60d407dd4b246bee063b4af231a59c13de80615
ramiromd <ramiromd@DESKTOP-4MP4LE0>
2023-05-02 06:58:11 -0600

    Se agregan los ficheros 13_modules.py, my_module.py, y el directorio __pycache__

---
+++ 13_modules.py
@@ -0,0 +1,20 @@
+### Modules ###
+
+from math import pi as PI_VALUE
+import math
+from my_module import sumValue, printValue
+import my_module
+
+my_module.sumValue(5, 8, 9)
+my_module.printValue("Hola python")
+
+
+sumValue(1, 2, 3)
+printValue("Hola python")
+
+
+print(math.e)
+print(math.pow(2, 8))
+
+
+print(PI_VALUE)
